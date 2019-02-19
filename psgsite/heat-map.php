c11620b56c06ae5a3d7991ee98289c2931b527c0
PaoDLR <36217674+PaoDLR@users.noreply.github.com>
2019-02-19 13:51:35 +0800

    heat-map initial layout of the page

---
+++ psgsite/heat-map.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <base target="_top">
+    <style>
+        
+        .headerCSS{
+            font-family: Century Gothic;
+            display: flex;
+            align-items: center;
+        }
+        
+        .headerCSS img{
+            flex-grow: 0px;
+            flex-shrink: 0px;
+        }
+        
+        .headerText{
+            margin-left: 15px;
+            font-family: Century Gothic;
+            font-size: 20px;
+        }
+        
+        .searchDate{
+            margin-left: 500px;
+        }
+        
+        #submitBtn{
+            height: 25px;
+            width: auto;
+        }
+        
+    </style>
+  </head>
+  <body>
+      
+     <div class="headerCSS">
+         <img src="https://lh3.googleusercontent.com/4uDjlPVjSID_i590xMHU-ZyK_DGY8rHxO7wexYJXTOQvSuR8I7_-n03KMVVQKXvBAAmgK5kmvnq53ikUEyQd6G_irLVZ5_Axxq9cKiLNKFYp6RmmeeE2EF6iUpe30Z86DtUrhJ5AzHetdP9Pa7vTAbngR70OuIZln9mc68ylpZtc0AvawW1B7HyXkIjrGZhdAl1XoPlIonVMGbhceZh6v7MogYbgf7nkiRjoOOwtJuwfMxy0mBdnkgFGos165Fcnl5iGF_aGaaLIVLmGHgIGNOqDoMotSCfCl33_t33HooJBqkVgwh_dCEl9G1r0xEaitkuWqEpeOAZyopoqaW8fSHpxJn9eNi5OWYk8iu3DRQkW-AYKAkdgRCPNo9LqubsKt3U7grk31uany7b0yuFNcVJRy4p3tnL7FGOE9RQxKEHSDUwUI29r-6jOx8NfXiD2Vv4r1VnMXJQqtMNgbI0hpVxMg9PJynRm6gy-n7Y_9Nrlwv_-W8mjF55MRFTDWqzVbOo6WzPZf1v8RDW75Mz4AzvLYyI2i__FY2NlXqb_jmbSSztpSBD03qUcsqhgQqiWA3IEE2-f0aBUewTiZ9Mh5SrNU0OfwgQZThn7s45_z7xwr--roI4iwqIUzQLbJzt1M-6vEZZX1XR42AUxogrfzBxxE2iiiZdxHXw=s128-no?.png" height="50" width="50">
+         <div class="headerText">
+            <p>Heat Map</p>
+         </div>
+         <div class="searchDate">
+            <input type="date"><button id="submitBtn" type="submit" value="Submit">Submit</button>
+         </div>
+      </div>
+      <hr>
+  </body>
+</html>
