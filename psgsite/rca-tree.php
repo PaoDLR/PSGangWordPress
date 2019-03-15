d1b4d73e5aa759c64bac178287e202027c69797c
Josh del Barrio <36443156+Jdelbz@users.noreply.github.com>
2019-03-15 10:50:44 +0800

    removed unnecessary modal script

--- psgsite/rca-tree.php
+++ psgsite/rca-tree.php
@@ -5,11 +5,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <script>
-        //prevent closing the modal when clicking outside it
-        $('#myModal').modal({
-            backdrop: 'static',
-            keyboard: false
-        })
         
         //current node clicked, used for storing subnode value purposes
         var currentNode;
